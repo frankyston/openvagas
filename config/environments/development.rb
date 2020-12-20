9ea1e128798d413b44e7cc51289637024a622295
Frankyston Lins <frankyston@gmail.com>
2020-12-19 21:56:03 -0300

    Configuration gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
