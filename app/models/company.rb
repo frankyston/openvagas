c22a98d1a8e50bdfd6f1a305d1d6a47b709098d1
Frankyston Lins <frankyston@gmail.com>
2020-12-30 22:16:37 -0300

    Add CRUD of Positions

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+  has_many :positions
   has_one_attached :logo
   validates :name, :url, presence: true
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,9 @@
 class Position < ApplicationRecord
   belongs_to :company
+
+  enum career: [:developer, :business_inteligence, :information_technology, :design, 
+                :product, :technology, :other]
+  enum contract: [:clt, :pj, :match]
+
+  validates :name, :career, :contract, :city, :state, :summary, :description, presence: true
 end
