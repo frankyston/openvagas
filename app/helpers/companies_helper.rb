83dff1b37238761399aa69ffe1ceee31c56e18ec
Frankyston Lins <frankyston@gmail.com>
2020-12-30 21:07:02 -0300

    Bugfix new company and default value on career and contract of model position

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,6 +1,6 @@
 module CompaniesHelper
   def dynamic_url_company
-    if current_user.company.present?
+    if current_user.company.present? && current_user.company.try(:id).present?
       edit_company_path(current_user.company)
     else
       new_company_path
