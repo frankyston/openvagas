cfab049dfca396460c23b798d549dd44a295c750
Frankyston Lins <frankyston@gmail.com>
2021-01-08 23:45:13 -0300

    Adding action text; improve form position; create action update company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -19,6 +19,13 @@ class CompaniesController < ApplicationController
   end
 
   def update
+    @company = current_user.company
+    if @company.update(company_params)
+      flash[:notice] = 'Empresa atualizada com sucesso.'
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -51,23 +51,9 @@ class PositionsController < ApplicationController
 
   def set_i18n_careers
     @careers = I18n.t('activerecord.attributes.position.careers')
-    # @careers = []
-    # Position.careers.each_with_index do |career, index|
-    #   @careers << [
-    #     Position.careers.to_a[index].first,
-    #     I18n.t('activerecord.attributes.position.careers').values.to_a[index]
-    #   ]
-    # end
   end
 
   def set_i18n_contracts
     @contracts = I18n.t('activerecord.attributes.position.contracts')
-    # @contracts = []
-    # Position.contracts.each_with_index do |career, index|
-    #   @contracts << [
-    #     Position.contracts.to_a[index].first,
-    #     I18n.t('activerecord.attributes.position.contracts').values.to_a[index]
-    #   ]
-    # end
   end
 end
